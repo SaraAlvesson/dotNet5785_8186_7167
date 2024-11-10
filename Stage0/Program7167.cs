049b8ee8ef2983284588f2a65fdeffeced0ab357
nblau <namablau@gmail.com>
2024-11-10 15:25:05 +0200

    Initial Code+Welcome Functions

---
+++ Stage0/Program7167.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    { static partial void Welcome7167()
+        { 
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
